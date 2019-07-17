0e4e5c2824de96fa92b704e7f434f8f45dca69a6
oneweekstudio <48544624+oneweekstudio@users.noreply.github.com>
2019-07-17 13:31:44 +0700

    fix import firebase

--- SMRemote/Classes/SMRemote.swift
+++ SMRemote/Classes/SMRemote.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+import FireBase
+
+class SMRemote : NSObject {
+    
+    static let prefix:String = "_counter"
+    var remoteConfig = RemoteConfig.remoteConfig()
+    var settings = RemoteConfigSettings()
+    var expirationDuration = 3600
+    
+    static let remote : SMRemote = {
+        let instance = SMRemote()
+        instance.settings.minimumFetchInterval = 0
+        instance.remoteConfig.configSettings = instance.settings
+//        instance.remoteConfig.setDefaults(fromPlist: "RemoteConfigDefaults")
+        return instance
+    }()
+    
+}
