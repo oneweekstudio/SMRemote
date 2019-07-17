05983388a877edeb67063a3fbcfb98d432a0d260
oneweekstudio <48544624+oneweekstudio@users.noreply.github.com>
2019-07-17 13:23:35 +0700

    Add model config

---
+++ SMRemote/Classes/SMConfig.swift
@@ -0,0 +1,16 @@
+//
+//  SMConfig.swift
+//  FirebaseCore
+//
+//  Created by OW01 on 7/17/19.
+//
+
+import Foundation
+class SMConfig : NSObject {
+    
+    var ad_dialog_start = 1
+    var ad_dialog_loop = 1
+    var more_tool = 1
+
+    
+}
