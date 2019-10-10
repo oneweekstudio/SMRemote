40f8862249734d2ad73b241d9215cecd1085003c
oneweekstudio <oneweekstudio@gmail.com>
2019-10-10 16:46:20 +0700

    update

---
+++ Example/Pods/Target Support Files/MaterialComponents/MaterialComponents-umbrella.h
@@ -0,0 +1,78 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "CAMediaTimingFunction+MDCAnimationTiming.h"
+#import "MaterialAnimationTiming.h"
+#import "UIView+MDCTimingFunction.h"
+#import "MaterialButtons.h"
+#import "MDCButton.h"
+#import "MDCFlatButton.h"
+#import "MDCFloatingButton+Animation.h"
+#import "MDCFloatingButton.h"
+#import "MDCRaisedButton.h"
+#import "MaterialCards.h"
+#import "MDCCard.h"
+#import "MDCCardCollectionCell.h"
+#import "UICollectionViewController+MDCCardReordering.h"
+#import "MaterialDialogs.h"
+#import "MDCAlertController+ButtonForAction.h"
+#import "MDCAlertController.h"
+#import "MDCAlertControllerView.h"
+#import "MDCDialogPresentationController.h"
+#import "MDCDialogTransitionController.h"
+#import "UIViewController+MaterialDialogs.h"
+#import "MaterialInk.h"
+#import "MDCInkGestureRecognizer.h"
+#import "MDCInkTouchController.h"
+#import "MDCInkView.h"
+#import "MaterialShadowElevations.h"
+#import "MDCShadowElevations.h"
+#import "MaterialShadowLayer.h"
+#import "MDCShadowLayer.h"
+#import "MaterialShapesNew.h"
+#import "MDCCornerTreatmentNew.h"
+#import "MDCEdgeTreatmentNew.h"
+#import "MDCPathGeneratorNew.h"
+#import "MDCRectangleShapeGeneratorNew.h"
+#import "MDCShapedShadowLayerNew.h"
+#import "MDCShapedViewNew.h"
+#import "MDCShapeGeneratingNew.h"
+#import "MaterialTabs.h"
+#import "MDCTabBar.h"
+#import "MDCTabBarAlignment.h"
+#import "MDCTabBarIndicatorAttributes.h"
+#import "MDCTabBarIndicatorContext.h"
+#import "MDCTabBarIndicatorTemplate.h"
+#import "MDCTabBarItemAppearance.h"
+#import "MDCTabBarTextTransform.h"
+#import "MDCTabBarUnderlineIndicatorTemplate.h"
+#import "MDCTabBarViewController.h"
+#import "MaterialTypography.h"
+#import "MDCFontTextStyle.h"
+#import "MDCTypography.h"
+#import "UIFont+MaterialSimpleEquality.h"
+#import "UIFont+MaterialTypography.h"
+#import "UIFontDescriptor+MaterialTypography.h"
+#import "MaterialApplication.h"
+#import "UIApplication+AppExtensions.h"
+#import "MaterialIcons.h"
+#import "MDCIcons+BundleLoader.h"
+#import "MDCIcons.h"
+#import "MaterialIcons+ic_check_circle.h"
+#import "MaterialKeyboardWatcher.h"
+#import "MDCKeyboardWatcher.h"
+#import "MaterialMath.h"
+#import "MDCMath.h"
+
+FOUNDATION_EXPORT double MaterialComponentsVersionNumber;
+FOUNDATION_EXPORT const unsigned char MaterialComponentsVersionString[];
+
