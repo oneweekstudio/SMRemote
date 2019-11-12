9d1149876ff8c7f41cf560133a90764221dcc8ad
oneweekstudio <oneweekstudio@gmail.com>
2019-11-12 11:56:17 +0700

    fix SMRemote config

--- SMRemote/Classes/SMRemoteConfig.swift
+++ SMRemote/Classes/SMRemoteConfig.swift
@@ -8,9 +8,9 @@
 import Foundation
 open class SMRemoteConfig : NSObject {
     
-    @objc open var ad_dialog_start = 1
-    @objc open var ad_dialog_loop = 1
-    @objc open var more_tool = 1
+//    @objc open var ad_dialog_start = 1
+//    @objc open var ad_dialog_loop = 1
+//    @objc open var more_tool = 1
     
     @objc open var default_start = 1
     @objc open var default_loop = 2
